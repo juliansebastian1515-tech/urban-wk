206767c371f2cb2cc45da335be9fa2612d4851c6
juliansebastian1515-tech <juliansebastian1515@gmail.com>
2026-06-16 09:59:48 -0500

    script.js

--- script.js
+++ script.js
@@ -143,3 +143,120 @@ function showNotification(msg) {
         notif.remove();
     }, 2000);
 }
+function mostrarQR() {
+
+    const metodo =
+        document.getElementById("metodoPago").value;
+
+    const qr =
+        document.getElementById("contenedorQR");
+
+    if (metodo === "Nequi") {
+
+        qr.innerHTML = `
+            <h4>Escanea y paga por Nequi</h4>
+            <img src="imagenes/nequi.jpg"
+                 width="250">
+        `;
+
+    } else if (metodo === "Bancolombia") {
+
+        qr.innerHTML = `
+            <h4>Escanea y paga por Bancolombia</h4>
+            <img src="imagenes/bancolombia.jpg"
+                 width="250">
+        `;
+
+    } else if (metodo === "Daviplata") {
+
+        qr.innerHTML = `
+            <h4>Escanea y paga por Daviplata</h4>
+            <img src="imagenes/daviplata.jpg"
+                 width="250">
+        `;
+
+    } else {
+
+        qr.innerHTML = "";
+    }
+}
+
+function enviarPedidoWhatsapp() {
+
+    const nombre =
+        document.getElementById('clienteNombre').value;
+
+    const telefono =
+        document.getElementById('clienteTelefono').value;
+
+    const direccion =
+        document.getElementById('clienteDireccion').value;
+
+    const ciudad =
+        document.getElementById('clienteCiudad').value;
+
+    const pago =
+        document.getElementById('metodoPago').value;
+
+    if(
+        !nombre ||
+        !telefono ||
+        !direccion ||
+        !ciudad ||
+        !pago
+    ){
+        alert("Completa todos los campos");
+        return;
+    }
+
+    let total = 0;
+
+    let mensaje =
+        `*NUEVO PEDIDO URBAN W&K*%0A%0A`;
+
+    mensaje +=
+        `👤 Cliente: ${nombre}%0A`;
+
+    mensaje +=
+        `📱 Teléfono: ${telefono}%0A`;
+
+    mensaje +=
+        `📍 Dirección: ${direccion}%0A`;
+
+    mensaje +=
+        `🏙️ Ciudad: ${ciudad}%0A`;
+
+    mensaje +=
+        `💳 Pago: ${pago}%0A%0A`;
+
+    mensaje +=
+        `🛒 PRODUCTOS:%0A`;
+
+    cart.forEach(item => {
+
+        mensaje +=
+            `• ${item.name} x${item.quantity}%0A`;
+
+        total +=
+            item.price * item.quantity;
+    });
+
+    mensaje +=
+        `%0A💰 Total: $${total.toLocaleString()} COP`;
+
+    mensaje +=
+        `%0A%0AAdjunto comprobante de pago.`;
+
+    window.open(
+        `https://wa.me/573142043137?text=${mensaje}`,
+        '_blank'
+    );
+
+    cart = [];
+
+    localStorage.removeItem('cart');
+
+    updateCart();
+
+    document.getElementById('checkout-form').style.display = 'none';
+}
